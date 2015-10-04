1857b26a45460db2d89d4d37ba7df1be7ad38ed5
Arne Mæhlum <arne@iterate.no>
2015-10-04 16:24:41 +0200

    Init

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Raphael from 'webpack-raphael';
+
+import '../sass/HelloForm.scss';
+
+
+class RaphaelRoot extends React.Component {
+
+	render() {
+		const {paper} = this.props;
+
+		const width = paper.canvas.offsetWidth;
+		const height = paper.canvas.offsetHeight;
+
+		const x = width / 2;
+		const y = height / 2;
+		const r = Math.min(width, height) / 4;
+
+		console.log('Size:', width, '*', height);
+
+
+		paper.circle(x, y, r)
+			.attr({ stroke: '#fff', fill: '#5aa' });
+
+		// return false;
+	}
+}
+
+window.onload = function() {
+	const paper = new Raphael('raphael-paper', '100%', 500);
+
+	const test = new RaphaelRoot({ paper }).render();
+};
