823a483d957d4ae29cf2f80d3df76aedb9f8209c
Arne Mæhlum <arne@iterate.no>
2015-10-05 16:52:43 +0200

    asdf

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,70 @@
-function getEntrySources(sources) {
-    if (process.env.NODE_ENV !== 'production') {
-        sources.push('webpack-dev-server/client?http://localhost:8080');
-        sources.push('webpack/hot/only-dev-server');
-    }
+'use strict';
 
-    return sources;
-}
+var webpack = require('webpack'),
+    path    = require('path');
+
+var APP_PATH        = path.join(__dirname, '/src'),
+    DIST_PATH       = path.join(APP_PATH, '/dist'),
+    FRONTEND_PATH   = path.join(APP_PATH, '/frontend');
 
 module.exports = {
+    // cache: true,
+
     entry: {
-        main: getEntrySources([
-            './scripts/main.js'
-        ])
+        main: [
+            path.join(FRONTEND_PATH, '/main.js')
+        ]
     },
     output: {
-        publicPath: '/dist/',
-        filename: '[name].js'
+        path 		: DIST_PATH,
+        publicPath  : '/dist/',
+        filename    : '[name].js'
+    },
+    resolve: {
+        root: FRONTEND_PATH,
+        extensions: ['', '.jsx', '.js']
     },
     module: {
         loaders: [
             { test: /\.js$/, loaders: ['react-hot', 'jsx', 'babel'], exclude: /node_modules/ },
             { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
         ]
-    }
+        // loaders: [
+        //     {
+        //         test: /\.jsx?$/,
+        //         exclude: /node_modules/,
+        //         loader: [
+        //             'react-hot',
+        //             'babel?optional[]=es7.classProperties&optional[]=es7.objectRestSpread',
+        //             'eslint'
+        //         ].join('!')
+        //     },
+        //     { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
+        // ]
+    },
+    // externals: {
+    //     react: 'React'
+    // },
+    // plugins: [
+    //     new webpack.DefinePlugin({
+    //         'process.env': {
+    //             'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    //         }
+    //     })
+    // ],
+    // devServer: {
+    //     host    : 'localhost',
+    //     port    : '1234',
+    //     quiet   : false,
+    //     noInfo  : false,
+    //     proxy: {
+    //         '*': 'http://localhost:3001/'
+    //     },
+    //     stats: {
+    //         chunks      : false,
+    //         chunkModules: false,
+    //         timings     : true
+    //     }
+    // }
 };
+
