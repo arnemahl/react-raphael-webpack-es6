545d572e9fce48d74e65fded84484d5f84a516b7
Arne Mæhlum <arne@iterate.no>
2015-10-06 16:21:26 +0200

    asdflkjasf

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -7,33 +7,36 @@ console.log('Loaded');
 
 class RaphaelRoot extends React.Component {
 
-	render() {
-		const {paper} = this.props;
+    render() {
+        
+        console.log('HELLO!!');
+    
+        const {paper} = this.props;
 
-		const width = paper.canvas.offsetWidth;
-		const height = paper.canvas.offsetHeight;
+        const width = paper.canvas.offsetWidth;
+        const height = paper.canvas.offsetHeight;
 
-		const i = true
-			? 5
-			: 0;
+        const i = true
+            ? 5
+            : 0;
 
-		console.log(i);
+        console.log(i);
 
-		const x = width / 2;
-		const y = height / 2;
-		const r = Math.min(width, height) / 4;
+        const x = width / 2;
+        const y = height / 2;
+        const r = Math.min(width, height) / 4;
 
-		console.log('Size:', width, '*', height);
+        console.log('Size:', width, '*', height);
 
-		paper.circle(x, y, r)
-			.attr({ stroke: '#fff', fill: '#5aa' });
+        paper.circle(x, y, r)
+            .attr({ stroke: '#fff', fill: '#5aa' });
 
-		// return false;
-	}
+        // return false;
+    }
 }
 
 window.onload = function() {
-	const paper = new Raphael('raphael-paper', '100%', 500);
+    const paper = new Raphael('raphael-paper', '100%', 500);
 
-	new RaphaelRoot({ paper }).render();
+    new RaphaelRoot({ paper }).render();
 };
